a87ee39000d5efb84b3fc4ed1bb223b2eada5c5b
Xuke Fang <xuke@fb.com>
2022-06-28 12:47:12 -0700

    Initial Commit

---
+++ agora-test/agora_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  agora_testApp.swift
+//  agora-test
+//
+//  Created by Xuke Fang on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct agora_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
